6166b2edf30d73b7e0c309f3c6f3ad02fe6b1c2e
Rafaboom <rafaella.santos.carvalho@escola.pr.gov.br>
2025-08-01 09:52:41 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,52 @@
+criaCartao(
+    'História',
+    'Quem foi o líder da União Soviética durante a Segunda Guerra Mundial?',
+    'Josef Stalin'
+)
+
+criaCartao(
+    'História',
+    'Qual foi o nome do muro que dividiu Berlim durante a Guerra Fria?',
+    'Muro de Berlim'
+)
+
+criaCartao(
+    'História',
+    'Qual explorador europeu é creditado por ter chegado às Américas em 1492?',
+    'Cristóvão Colombo'
+)
+
+criaCartao(
+    'História',
+    'Em que país o Apartheid foi implementado?',
+    'África do Sul'
+)
+
+criaCartao(
+    'História',
+    'Qual foi a dinastia que construiu a Grande Muralha da China?',
+    'Dinastia Qin/Ming'
+)
+
+criaCartao(
+    'História',
+    'Qual foi a primeira civilização a desenvolver a escrita cuneiforme?',
+    'Sumérios'
+)
+
+criaCartao(
+    'Geografia',
+    ' Depressões são formas de relevo rebaixadas que se formaram a partir de um longo processo de erosão.',
+)
+
+criaCartao(
+    'Geografia',
+    'O que é latitude e longitude? Explique.',
+    'A latitude é a medida em graus de qualquer ponto da superfície terrestre até a Linha do Equador (0 a 90 graus - norte e sul). A longitude é a medida em graus de qualquer ponto da superfície terrestre até o Meridiano de Greenwich (0 a 180 graus - leste e oeste). '
+)
+
+criaCartao(
+    'Geografia',
+    'O que é geografia? ',
+    'É  a ciência que estuda a Terra, o espaço geográfico e as relações entre a sociedade e o meio ambiente.'
+)

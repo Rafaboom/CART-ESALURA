8743a76e04098fae955e2d7e5b0bcbf482e0757a
Rafaboom <rafaella.santos.carvalho@escola.pr.gov.br>
2025-08-05 08:16:17 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -50,3 +50,130 @@ criaCartao(
     'O que é geografia? ',
     'É  a ciência que estuda a Terra, o espaço geográfico e as relações entre a sociedade e o meio ambiente.'
 )
+
+criaCartao(
+    'Geografia',
+    'Em qual continente fica a Austrália?',
+    'Oceania'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais países fazem parte dos BRICS?',
+    'Brasil, Rússia, Índia, China e África do Sul.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o país mais rico do mundo, com base no PIB?',
+    'Estados Unidos.'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 5!? ',
+    '120'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 3! + 4!? ',
+    '30'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula o perímetro de um quadrado?',
+    'Soma de todos os lados'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a área de um quadrado ',
+    'Multiplica a medida de um lado do quadrado por ela mesma.'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual a raiz quadrada de 64?',
+    '8.'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual a raiz quadrada de 169?',
+    '13'
+)
+
+criaCartao(
+    'Esporte',
+    'Qual é o esporte mais popular do mundo?',
+    'Futebol'
+)
+
+criaCartao(
+    'Esporte',
+    'Quantos jogadores compõem um time de basquete em quadra?',
+    '5 jogadores'
+)
+
+criaCartao(
+    'Esporte',
+    'Qual país sediou os primeiros Jogos Olímpicos da era moderna em 1896?',
+    'Grécia (na cidade de Atenas)'
+)
+
+criaCartao(
+    'Esporte',
+    'No tênis, quantos sets são necessários para vencer uma partida em Grand Slams masculinos? ',
+    '3 sets (melhor de 5 sets)'
+)
+
+criaCartao(
+    'Esporte',
+    'Qual modalidade de natação é conhecida como "o estilo mais rápido"?    ',
+    'Crawl (ou nado livre)'
+)
+
+criaCartao(
+    'Esporte',
+    'Qual esporte utiliza um taco para impulsionar uma bola pequena em buracos no campo?',
+    'Golfe'
+)
+
+
+criaCartao(
+    'Filmes e Séries',
+    'No filme ‘’Os Vingadores’’ de 2012, quem é o vilão?    ',
+    'Loki'
+)
+
+criaCartao(
+    'Filmes e Séries',
+    'Na saga de Jogos Vorazes, quem foi o mentor de Katniss Everdeen?',
+    ' Haymitch'
+)
+
+criaCartao(
+    'Filmes e Séries',
+    'Qual filme é conhecido por um navio colidir com um iceberg?    ',
+    'Titanic'
+)
+
+criaCartao(
+    'Filmes e Séries',
+    'No filme ‘’Shrek’’, qual a maldição da Fiona?    ',
+    'De dia é uma princesa, e de noite uma ogra. '
+)
+
+criaCartao(
+    'Filmes e Séries',
+    ' O que Alfred é do Batman?    ',
+    'Mordomo. '
+)
+
+criaCartao(
+    'Filmes e Séries',
+    '  Na saga de ‘’Jurassic World’’, qual o nome do dinossauro velociraptor de Owen?    ',
+    'Blue. '
+)
